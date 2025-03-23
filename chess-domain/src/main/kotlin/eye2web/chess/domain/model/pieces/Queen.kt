ec727f41e8fe4a8a169ae351684309f9b707f37a
Remco van der Heijden <remco.vanderheijden@pinkroccade.nl>
2025-03-23 14:53:16 +0100

    Queen and Pawn placeholder

---
+++ chess-domain/src/main/kotlin/eye2web/chess/domain/model/pieces/Queen.kt
@@ -0,0 +1,16 @@
+package eye2web.chess.domain.model.pieces
+
+import eye2web.chess.domain.Board
+import eye2web.chess.domain.model.Color
+import eye2web.chess.domain.model.pieces.base.BasePiece
+import eye2web.chess.domain.model.position.Position
+
+class Queen(
+    override val color: Color,
+    override var position: Position?,
+    override var isFirstMove: Boolean
+) : BasePiece() {
+    override fun addValidMoves(validPositions: MutableList<Position>, board: Board) {
+        TODO("Not yet implemented")
+    }
+}
